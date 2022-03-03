8ded2bba2430fe2af662f761f28797ddb5c67e3c
Stephen Buchanan <stephen.buchanan@random.studio>
2022-03-03 11:07:10 +0100

    better defaults

--- src/config/defaults.ts
+++ src/config/defaults.ts
@@ -2,15 +2,18 @@ import path from "path";
 import { Config } from "./types";
 
 const defaults: Config = {
-  tether: {},
+  tether: {
+    host: "localhost",
+  },
   agentType: "lidarConsolidation",
   loglevel: "info",
   numLidars: 1,
   lidarConfigPath: path.resolve(__dirname, "..", "..", "dist", "lidars.json"),
   clustering: {
-    neighbourhoodRadius: 250,
+    neighbourhoodRadius: 200,
     minNeighbours: 3,
-  }
+    maxClusterSize: 2500,
+  },
 };
 
 export default defaults;
